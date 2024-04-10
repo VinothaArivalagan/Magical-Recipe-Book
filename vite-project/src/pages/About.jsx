ff6110382b798bb353eb9d4c973273785b35861d
Celso Gando <Celsogando7@gmail.com>
2024-04-10 09:33:00 +0200

    About Page is done

--- vite-project/src/pages/About.jsx
+++ vite-project/src/pages/About.jsx
@@ -1,9 +1,30 @@
 function About() {
   return (
     <div className="about">
-      <h2>About</h2>
+      <h1>About</h1>
+      <h2>Description</h2>
+
+<h3>Welcome to our Magical cooking recipe book app!</h3>
+
+<h3>Purpose:</h3>
+Our app aims to simplify your culinary journey by providing a vast collection of mouthwatering recipes.
+Whether you’re a seasoned chef or a kitchen newbie, our app is your trusted companion for creating delicious meals.
+
+<h3>Features:</h3>
+Recipe Search: Easily find recipes based on ingredients, cuisine, dietary preferences, and cooking time.
+Step-by-Step Instructions: Clear instructions with images for each recipe.
+
+<h3>Favorites and Collections: </h3>
+
+Meal Planner: Plan your weekly meals effortlessly.
+
+<h3>What Sets Us Apart?</h3>
+
+Interactive Cooking Mode:Follow recipes hands-free.
+Perfect for messy kitchen situations!
+
       <p>Project Description: The Project is about Recipes</p>
-      <p>Team members - CELSO AND VINOTHA </p>
+      <h3>Team members </h3>- CELSO(frontend) AND VINOTHA(backend) 
     </div>
   );
 }
